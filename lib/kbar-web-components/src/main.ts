c2d240cc243a51b4744be243137db22dbb9b9d33
Krystof <krystof.kratky2003@gmail.com>
2024-09-05 01:56:25 +0200

    feat: example apps

--- lib/kbar-web-components/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { setupCounter } from './counter.ts';
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>hello world</div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
